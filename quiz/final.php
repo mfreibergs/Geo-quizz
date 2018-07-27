600687005c9762e2c82dacf6e977b5c3e87b1252
mfreibergs <41774986+mfreibergs@users.noreply.github.com>
2018-07-27 19:15:58 +0300

    Update final.php

--- quiz/final.php
+++ quiz/final.php
@@ -26,12 +26,10 @@
 	</header>
 	<main>
 		<!-- Apsveic lietotāju par testa izpildīšanu un parāda, cik daudz atbildes bija pareizas -->
-		<div class="container">
-			<h2 style="text-align: center;">You're Done!</h2>
-			<p style="text-align: center;">Congratulations, <?php echo $_SESSION['username']; ?>!<br> You have completed the test</p>
-			<h3 style="text-align: center;">Final Score: <?php echo $_SESSION['score']; ?> / <?php echo $total ?></h3>
-			<a href="index.php" class="start">Take Again</a>
-		</div>
+		<h2 style="text-align: center;">You're Done!</h2>
+		<p style="text-align: center;">Congratulations, <?php echo $_SESSION['username']; ?>!<br> You have completed the test</p>
+		<h3 style="text-align: center;">Final Score: <?php echo $_SESSION['score']; ?> / <?php echo $total ?></h3>
+		<a href="index.php" class="start">Take Again</a>
 	</main>
 
 </body>
