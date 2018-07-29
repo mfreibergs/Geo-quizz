adc18a7627b06d637d52a8bc96237657012eb34e
mfreibergs <41774986+mfreibergs@users.noreply.github.com>
2018-07-30 01:42:37 +0300

    Update final.php

--- quiz/final.php
+++ quiz/final.php
@@ -3,12 +3,19 @@
 <?php 
 
 	//	Iegūst kopējo jautājājumu skaitu
+	$username = $_SESSION['username'];
+	$score = $_SESSION['score'];
 	$qtype = $_SESSION['qType'];
+
 	$query = "SELECT * FROM `questions`
 				WHERE quiz_type = '$qtype'";
 
-	$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+	$result = $mysqli->query($query) or die($mysqli->error);
 	$total = $result->num_rows;
+
+	$sql = "INSERT INTO `users` (username, quiz_type, cor_ans)
+				VALUES ('$username', '$qtype', '$score');";
+	$insert_user = $mysqli->query($sql) or die($mysqli->error);
 ?>
 <!doctype html>
 <html>
@@ -22,14 +29,15 @@
 <body>
 	
 	<header>
-		<h1>Geography Quiz</h1>
+			<h1>Geography Quiz</h1>
 	</header>
+	<?php ?> 
 	<main>
 		<!-- Apsveic lietotāju par testa izpildīšanu un parāda, cik daudz atbildes bija pareizas -->
-		<h2 style="text-align: center;">You're Done!</h2>
-		<p style="text-align: center;">Congratulations, <?php echo $_SESSION['username']; ?>!<br> You have completed the test</p>
-		<h3 style="text-align: center;">Final Score: <?php echo $_SESSION['score']; ?> / <?php echo $total ?></h3>
-		<a href="index.php" class="start">Take Again</a>
+			<h2 style="text-align: center;">You're Done!</h2>
+			<p style="text-align: center;">Congratulations, <?php echo $username; ?>!<br> You have completed the test</p>
+			<h3 style="text-align: center;">Final Score: <?php echo $score; ?> / <?php echo $total ?></h3>
+			<a href="index.php" class="start">Take Again</a>
 	</main>
 
 </body>
